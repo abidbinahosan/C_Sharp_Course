c6252607d20d04eca39b5f86b101422de2964204
Abid Bin Ahosan <abid.ahosan@brainstation-23.com>
2025-03-24 15:16:05 +0600

    Variables

---
+++ Variables.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            long longCount = 1000;
+            float salary = 1000.34f;
+            double rate = 23.45;
+            decimal interest = 12.34m;
+            char letter = 'A';
+            bool done = false;
+            string name = "Abid Bin Ahosan!";
+
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(longCount);
+            Console.WriteLine(salary);
+            Console.WriteLine(rate);
+            Console.WriteLine(interest);
+            Console.WriteLine(letter);
+            Console.WriteLine(done);
+            Console.WriteLine(name);
+
+        }
+    }
+}
