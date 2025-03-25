bbd7700f0f98cb86fef2a826aa0e7ceff64443fd
Abid Bin Ahosan <abid.ahosan@brainstation-23.com>
2025-03-25 11:55:39 +0600

    Update Exercise 03.cs

--- Exercise 03.cs
+++ Exercise 03.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 /*
- * Excersice 02
+ * Excersice 03
  Write a program and ask the user to enter 5 numbers. If a number has been previously entered, display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them and display the result on the console. */
 namespace C_Sharp_Course
 {
